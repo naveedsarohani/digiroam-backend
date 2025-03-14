666b4bdec46e99233f9d75a4f1dd4bd36291f659
naveedsarohani <naveed.sarohani@gmail.com>
2025-03-14 17:55:27 +0500

    Develop email alerts for esim users

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -297,8 +297,8 @@ const changeCurrentPassword = async (req, res, next) => {
     user.password = newPassword;
     await user.save({ validateBeforeSave: false });
 
-    res.status(200).json(new ApiResponse(200, {}, "Password changed successfully"));
     await sendPasswordChangeEmail(user);
+    return res.status(200).json(new ApiResponse(200, {}, "Password changed successfully"));
   } catch (error) {
     next(error);
   }
