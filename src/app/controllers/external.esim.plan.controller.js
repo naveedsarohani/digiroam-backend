818d736ae4bf7925e8f53c4e8b52e7bf6c324081
naveedsarohani <naveed.sarohani@gmail.com>
2025-04-17 13:22:35 +0500

    Modify the route to tacke cors

--- src/app/controllers/external.esim.plan.controller.js
+++ src/app/controllers/external.esim.plan.controller.js
@@ -1,15 +1,15 @@
 import axiosInstance from "../../utils/helpers/axios.instance.js";
 import modifyPackagePrices from "../../utils/helpers/modify.package.prices.js";
 
-const index = async (req, res, next) => {
+const index = async (req, res) => {
     try {
         const packages = await axiosInstance.post("/package/list", { ...req.body });
         if (packages?.data?.success === false) throw new Error(packages.data?.errorMsg);
 
         const transformData = modifyPackagePrices(packages.data.obj);
-        return res.responss(200, "Data packages fetched successfully", { data: transformData });
+        return res.response(200, "Data packages fetched successfully", { data: transformData });
     } catch (error) {
-        next(error);
+        return res.response(500, "Internal server error", { error: error.message });
     }
 };
 
