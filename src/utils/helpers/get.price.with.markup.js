b76f9d6062ec74c8010fd2971bbf674182705cd0
muqadasqk <zainaliqk@gmail.com>
2025-04-27 18:16:08 +0500

    Paypal payment for native

--- src/utils/helpers/get.price.with.markup.js
+++ src/utils/helpers/get.price.with.markup.js
@@ -1,5 +1,5 @@
-const getPriceWithMarkup = (price, markup) => {
-    const markup = price * ((markup ?? 1) / 100);
+const getPriceWithMarkup = (price, markupPercentage) => {
+    const markup = price * ((markupPercentage ?? 1) / 100);
     return Number(price + markup).toFixed(2);
 };
 
