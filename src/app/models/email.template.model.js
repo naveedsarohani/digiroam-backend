9d6f16d1744431f03043b0fa5ed25bc7302f5c56
naveedsarohani <naveed.sarohani@gmail.com>
2025-06-06 12:24:34 +0500

    Design email templates for OTP and Temporary password

--- src/app/models/email.template.model.js
+++ src/app/models/email.template.model.js
@@ -5,7 +5,7 @@ const EmailSchema = new mongoose.Schema(
         eventName: {
             type: String,
             required: true,
-            enum: ["ON_LOGIN", "ON_PASSWORD_CHANGE", "ON_PURCHASE", "ON_CANCEL", "ON_USAGE_80", "ON_1D_VALIDITY", "ON_EXPIRED", "ON_DISCOUNT", "ON_ACTIVATION_REMINDER"],
+            enum: ["ON_SEND_OTP", "ON_LOGIN", "ON_PASSWORD_CHANGE", "ON_PURCHASE", "ON_CANCEL", "ON_USAGE_80", "ON_1D_VALIDITY", "ON_EXPIRED", "ON_DISCOUNT", "ON_ACTIVATION_REMINDER"],
         },
         subject: {
             type: String,
