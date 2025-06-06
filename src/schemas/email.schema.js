9c96c77ee36fc476f7f52836ec44d31470a62431
naveedsarohani <naveed.sarohani@gmail.com>
2025-06-06 14:27:51 +0500

    Design email templates for OTP and Temporary password

--- src/schemas/email.schema.js
+++ src/schemas/email.schema.js
@@ -1,6 +1,6 @@
 import Joi from "joi";
 
-const eventNames = ["ON_LOGIN", "ON_PASSWORD_CHANGE", "ON_PURCHASE", "ON_CANCEL", "ON_USAGE_80", "ON_1D_VALIDITY", "ON_EXPIRED", "ON_DISCOUNT", "ON_ACTIVATION_REMINDER"]
+const eventNames = ["ON_SEND_OTP", "ON_LOGIN", "ON_PASSWORD_CHANGE", "ON_PURCHASE", "ON_CANCEL", "ON_USAGE_80", "ON_1D_VALIDITY", "ON_EXPIRED", "ON_DISCOUNT", "ON_ACTIVATION_REMINDER"]
 
 const send = Joi.object({
     eventName: Joi.string()
